f4137bccfa2968beccb0139c08afd0d2e015f4cd
Nguyễn Xuân Phú <phu4723@gmail.com>
2021-05-05 13:15:28 +0700

    update pass

--- UploadAll-File/NGROK-CHECK.bat
+++ UploadAll-File/NGROK-CHECK.bat
@@ -2,7 +2,7 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator THTH-channel /add >nul
+net user administrator Nguyenphu0407. /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -15,7 +15,7 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-r
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels "
 echo User: administrator
-echo Pass: THTH-channel
+echo Pass: Nguyenphu0407.
 echo View more: https://youtube.com/c/tinhocthuchanh
 echo Visit Create VPS-RDP Part1: https://youtu.be/VnEJv49oKR8 , Part2: https://youtu.be/em221J1k520
 ping -n 10 127.0.0.1 >nul
